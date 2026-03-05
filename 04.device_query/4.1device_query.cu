80644e549308c4352ad60320a401e482296248e8
tam <tam19599tam@gmail.com>
2026-03-05 21:41:54 +0800

    Update 4.1device_query.cu

--- 04.device_query/4.1device_query.cu
+++ 04.device_query/4.1device_query.cu
@@ -1,36 +1,55 @@
 /*
-=======设备查询(获取GPU详细参数)=======
-Detected 1 CUDA Capable device(s)
+总结：
 
-Device 0: "NVIDIA GeForce GTX 1060"
-  Total amount of global memory:                 6144 MBytes (6442319872 bytes)   // 总显存大小， GPU的全局DRAM容量，供所有线程块共享访问。对于数据集较大的应用非常关键。
-  GPU Max Clock rate:                            1733 MHz (1.73 GHz)   // GPU核心的运行频率，更高的时钟频率通常意味着更快的执行速度，但也可能导致更高功耗。
-  L2 Cache Size:                                 1572864 bytes     // L2缓存是所有SM共享的高速缓存，能显著减少访问全局内存时的延迟。
-  Total amount of shared memory per block:       49152 bytes       // 一个block(线程块)中最多能用的shared memory(共享内存)的数量。
-  Total shared memory per multiprocessor:        98304 bytes       // 一个SM(相当于CPU中的核)可分配给多个线程块的总共享内存。
-  Total number of registers available per block: 65536             // 一个block中最多可用的寄存器总数，CUDA会自动按照线程分配。
-  Warp size:                                     32                // 一个warp线程的大小，同一个warp内线程同步执行。
-  Maximum number of threads per multiprocessor:  2048              // 一个SM同时可以托管的活跃线程数峰值，通常对应64warp。
-  Maximum number of threads per block:           1024              // 一个block中所支持的最大线程数量。
-  Max dimension size of a block size (x,y,z): (1024, 1024, 64)     // 一个block在 x、y、z 方向上的最大尺寸。三个维度的总乘积不能超过上面的block最大线程数量的限制。
-  Max dimension size of a grid size    (x,y,z): (2147483647, 65535, 65535)   // 一个grid在 x、y、z 方向上的最大尺寸。它决定了kernel launch时grid的大小。
+1.决定block大小看：
+                    maxThreadsPerBlock
+                    warpSize
+                    regsPerBlock
+                    sharedMemPerBlock
+一般选择：128 / 256 / 512
+
+2.决定 shared memory 用量看：
+                              sharedMemPerBlock
+                              sharedMemPerMultiprocessor
+否则block 上不去 → occupancy 低。
+
+3.决定kernel并发度看：
+                      maxThreadsPerMultiProcessor
+                      regsPerBlock
+                      sharedMemPerMultiprocessor
+
+能够直观确定每个SM能跑几个block。
+
+4.结合带宽测试程序：
+                      情况	                 说明
+                     带宽低	               访存瓶颈
+                     计算慢	               算力瓶颈
+                   occupancy低	           资源瓶颈
 
 */
 
 
 
-#include <cuda_runtime.h>
+#include <cuda_runtime.h>    // 提供cudaGetDeviceCount、cudaGetDeviceProperties、cudaSetDevice等API
 #include <cuda.h>
-#include <iostream>
+#include <stdio.h>
 #include <string>
 
+using namespace std;
 int main() 
 {
 
 
-    int deviceCount = 0;   // 初始化一个变量
-    cudaError_t error_id = cudaGetDeviceCount(&deviceCount);  // 获取当前机器的GPU数量, cudaError_t实际上是一个枚举类型，它所包含的每一个值都对应着CUDA操作可能出现的一种状态。
-    if (deviceCount == 0)   // 如果deviceCount数量等于0，说明没有可用的gpu
+    int deviceCount = 0;   // 初始化一个变量,用于存储机器中有几个可用的CUDA显卡的数量
+    cudaError_t error_id = cudaGetDeviceCount(&deviceCount);  // 获取当前机器可用的GPU数量, cudaError_t实际上是一个枚举类型，它所包含的每一个值都对应着CUDA操作可能出现的一种状态。
+    if (error_id != cudaSuccess)
+    {
+        printf("cudaGetDeviceCount failed: %s\n",cudaGetErrorString(error_id));
+        return -1;
+    }
+
+
+    if (deviceCount == 0)    // 如果deviceCount数量等于0，说明没有可用的gpu
     {
         printf("There are no available device(s) that support CUDA\n");
         return 0;
@@ -40,46 +59,48 @@ int main()
         printf("Detected %d CUDA Capable device(s)\n", deviceCount);
     }
 
-    for (int dev = 0; dev < deviceCount; ++dev)   // 遍历每一张GPU
+
+    for (int dev = 0; dev < deviceCount; ++dev)    // 从0号GPU开始，遍历每一张GPU
     {
-        cudaSetDevice(dev);   // 使用指定的GPU
+        cudaSetDevice(dev);   // 使用指定的GPU，例如0号GPU
         cudaDeviceProp deviceProp;  // 初始化当前device的属性获取对象
         cudaGetDeviceProperties(&deviceProp, dev);  // 把GPU设备编号dev的详细属性填充到deviceProp结构体中
 
         printf("\nDevice %d: \"%s\"\n", dev, deviceProp.name);   // 打印GPU设备编号dev和设备名称
         
-        // 打印显存总量（全局内存-Global Memory），总字节数deviceProp.totalGlobalMem除以1048576.0f转换为MB
+        // 打印全局显存总量，其直接影响能处理多大规模的问题，总字节数deviceProp.totalGlobalMem除以1048576.0f的含义是将字节B转换为MB
         printf("  Total amount of global memory:                 %.0f MBytes " "(%llu bytes)\n", static_cast<float>(deviceProp.totalGlobalMem / 1048576.0f),(unsigned long long)deviceProp.totalGlobalMem);
 
 
-        // 打印GPU最大时钟频率（每个SM的频率）---原始单位是kHz → 转换为MHz和 GHz
+        // 打印GPU最大时钟频率（每个SM的频率）---原始单位是kHz → 转换为MHz和 GHz，含义是GPU算术核心能够跑多块，类似CPU主频，该参数对计算密集型kernel影响大。
         printf( "  GPU Max Clock rate:                            %.0f MHz (%0.2f " "GHz)\n", deviceProp.clockRate * 1e-3f, deviceProp.clockRate * 1e-6f);
 
-        // 打印L2缓存(Cache)大小（多用于全局内存数据读写缓存）
+        // 打印L2缓存(Cache)大小（全GPU共享缓存，减少对全局内存的访问，对需要重复访问的数据很重要）
         printf("  L2 Cache Size:                                 %d bytes\n", deviceProp.l2CacheSize);
         
-        // ★：每个Block可用的Shared Memory总量（程序常用于线程间通信与局部缓存）
+        // ★：每个block中可用的Shared Memory总量（通常48k或96k），直接影响occupancy，occupancy指的是一个SM上实际运行的warp数/理论最大warp数。
         printf("  Total amount of shared memory per block:       %zu bytes\n", deviceProp.sharedMemPerBlock);
 
-        // ★：每个SM（Streaming Multiprocessor）上的共享内存总量
+        // ★：一个SM中的所有block的共享内存总上限，总量越大，一个SM能驻留更多block，并发度越高。
         printf("  Total shared memory per multiprocessor:        %zu bytes\n", deviceProp.sharedMemPerMultiprocessor);
 
-        // ★：每个Block可使用的寄存器数量（影响线程调度效率）
+        // ★：每个Block可使用的寄存器数量，regsPerThread(每个线程使用的寄存器数量) × threadsPerBlock(每个block中启动的线程数量=blockSize) ≤ regsPerBlock(硬件允许一个block最多使用的寄存器总数)
+        // ⚠：★一旦超出会降低occupancy，对性能影响极大。★
         printf("  Total number of registers available per block: %d\n", deviceProp.regsPerBlock);
 
-        // ★：Warp大小：通常是 32，表示同时调度的线程组大小
+        // ★：Warp大小：通常固定是 32线程，1个Warp是GPU执行指令最小的调度单位
         printf("  Warp size:                                     %d\n", deviceProp.warpSize);
 
-        // ★：每个SM最多能同时容纳的线程数。
+        // ★：每个SM最多能同时容纳的线程数，一般为2048，对应64个Warp，决定occupancy上限。
         printf("  Maximum number of threads per multiprocessor:  %d\n", deviceProp.maxThreadsPerMultiProcessor);
 
-        // ★：每个Block中线程数的上限
+        // ★：每个Block中线程数的上限，一般为1024
         printf("  Maximum number of threads per block:           %d\n", deviceProp.maxThreadsPerBlock);
 
-        // ★：每个Block的线程维度限制
+        // ★：每个Block的三维尺寸限制，通常情况：block.x ≤ 1024,block.y ≤ 1024,block.z ≤ 64并且三者总乘积≤ maxThreadsPerBlock，否则kernel 启动会失败，CUDA 运行时返回错误。
         printf("  Max dimension size of a block size(x,y,z):     (%d, %d, %d)\n", deviceProp.maxThreadsDim[0], deviceProp.maxThreadsDim[1], deviceProp.maxThreadsDim[2]);
 
-        // ★：整个Grid的网格布局限制
+        // ★：整个Grid的三维尺寸限制，通常情况：x方向极大（2^31-1）,y/z较小
         printf("  Max dimension size of a grid size(x,y,z):      (%d, %d, %d)\n", deviceProp.maxGridSize[0], deviceProp.maxGridSize[1], deviceProp.maxGridSize[2]);
 
     }
